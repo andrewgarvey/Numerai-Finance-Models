09e21dfc9e102fbdf9258459ef91d74a180b87bb
Andrew Garvey <andrewtgarvey@gmail.com>
2018-12-06 23:59:55 -0500

    some notes on numeria scoring

--- 1.0-ag-data_exploration.py
+++ 1.0-ag-data_exploration.py
@@ -19,7 +19,6 @@ import matplotlib.pyplot as plt
 # import other packages 
 
 
-
 #------------------------------------------------------------------------------
 # IMPORT FILES
 #setup dir
@@ -34,12 +33,63 @@ test = pd.read_csv('numerai_tournament_data.csv')
 os.chdir(outputdir)
 
 #------------------------------------------------------------------------------
+# I fully expect this to be boring stuff, they don't want us wasting time changing the data, they might even have a rule against it or something haha 
 
-# Basic stuff  
+'''
+ Lets talk about Numer.ai Scoring, and what it actually means! 
 
+1. Logarithmic Loss based scoring  (must be <0.693)
+       a- Validation Logloss: Graded against validation, publically displayed:  In our data this is "numerai_tournament_data.csv"
+       b- Test Logloss: This is a Hidden holdout set, it lets the know if they actually want to trust our model 
+       c- Live Logloss: This is IF they use our stuff, it's determines our payout along with our submitted stake.
+       
+For this project we only care about a. 
+          
+Score to benchmark ourselves against is Log loss <0.693: 
+    
+MATH ASIDE: 
+0.693 is recognizable as ln(2), so i did a quick check to see what they are actually asking for 
+
+-{(y*log(p) + (1 - y)*log(1 - p))} is the formula for Log Loss (for binary catorization)
+
+at p = 50% and y = 1  
+
+=-{(1*log(0.5)}
+= 0.693 
+
+same can be shown for y=0 and p =50% 
+
+Basically this just means you have to be better than Random guess as a benchmark!... but ofc it's not that easy
+
+2. Eras, there are many of them
+
+
+
+
+3. You must actually use the same model for everything, you can't just submit several overfitted models that each do well 
+
+
+
+
+
+
+
+'''
 train.describe().transpose()
 
 train.isna().sum() 
+
+eras = train.loc[:,'era']
+len(eras.unique())
+
+
+
+
+
+
+
+
+
 
 
 
